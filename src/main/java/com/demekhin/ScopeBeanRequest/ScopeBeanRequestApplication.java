1d78e73edbf9a4a386fc1f5008bbb173e4ae9f79
N <Nescafe.gold.1988@mail.ru>
2022-12-05 14:09:54 +0300

    trial version of the request scope sample

---
+++ src/main/java/com/demekhin/ScopeBeanRequest/ScopeBeanRequestApplication.java
@@ -0,0 +1,13 @@
+package com.demekhin.ScopeBeanRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScopeBeanRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScopeBeanRequestApplication.class, args);
+	}
+
+}
